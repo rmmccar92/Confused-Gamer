4d44839fdd9fc9406961d1ea0ff2efb3d3686bfe
Ryan McCarthy <rmmcc92@aol.com>
2021-07-19 22:22:50 -0400

    added event listeners for favorite buttons

---
+++ favoriteList.js
@@ -0,0 +1,52 @@
+var Button1 = $(".game-1-button");
+var Button2 = $(".game-2-button");
+var Button3 = $(".game-3-button");
+var Button4 = $(".game-4-button");
+var Button5 = $(".game-5-button");
+var Button6 = $(".game-6-button");
+var Button7 = $(".game-7-button");
+var Button8 = $(".game-8-button");
+var Button9 = $(".game-9-button");
+var Button10 = $(".game-10-button");
+
+var game1El = $(".game-1");
+var game2El = $(".game-2");
+var game3El = $(".game-3");
+var game4El = $(".game-4");
+var game5El = $(".game-5");
+var game6El = $(".game-6");
+var game7El = $(".game-7");
+var game8El = $(".game-8");
+var game9El = $(".game-9");
+var game10El = $(".game-10");
+
+Button1.on("click" , function () {
+    console.log(game1El.text());
+})
+Button2.on("click" , function () {
+    console.log(game2El.text());
+})
+Button3.on("click" , function () {
+    console.log(game3El.text());
+})
+Button4.on("click" , function () {
+    console.log(game4El.text());
+})
+Button5.on("click" , function () {
+    console.log(game5El.text());
+})
+Button6.on("click" , function () {
+    console.log(game6El.text());
+})
+Button7.on("click" , function () {
+    console.log(game7El.text());
+})
+Button8.on("click" , function () {
+    console.log(game8El.text());
+})
+Button9.on("click" , function () {
+    console.log(game9El.text());
+})
+Button10.on("click" , function () {
+    console.log(game10El.text());
+})
