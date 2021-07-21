3c6e86c4e23326b2ed0bea5f92b36c22fd7ae375
Aubri Henley <aubri.henley@gmail.com>
2021-07-21 14:34:32 -0400

    button click listener added

--- script.js
+++ script.js
@@ -13,48 +13,51 @@ var userInput;
 var top10listEl =$('#top-10-list');
 var platformButtonsEl= $('#platform-buttons');
 
-$.ajax({
-  type: 'GET',
-  dataType: 'jsonp',
-  crossDomain: true,
-  jsonp: 'json_callback',
-  url: 'https://www.giantbomb.com/api/games/?api_key=073c2f94ba69540e99d2b7e8b4cd3aebb2d9befb&format=jsonp',
+// $.ajax({
+//   type: 'GET',
+//   dataType: 'jsonp',
+//   crossDomain: true,
+//   jsonp: 'json_callback',
+//   url: 'https://www.giantbomb.com/api/games/?api_key=073c2f94ba69540e99d2b7e8b4cd3aebb2d9befb&format=jsonp',
 
 
-})
+// })
 
-  .then(function (data) {
-    console.log(data);
-  })
-submitButton.on("click", function () {
-  event.preventDefault();
-  userInput = inputEl.val();
-  console.log(userInput)
-  return userInput
-})
-
-var platformID = 146;
-var buttonSelection =  'https://www.giantbomb.com/api/games/?api_key=073c2f94ba69540e99d2b7e8b4cd3aebb2d9befb&format=jsonp&platforms=' + platformID;
+//   .then(function (data) {
+//     console.log(data);
+//   })
 
 
-$.ajax({
-  type: 'GET',
-  dataType: 'jsonp',
-  crossDomain: true,
-  jsonp: 'json_callback',
-  url: buttonSelection
-})
 
-.then(function(data){
-  console.log(data);
-});
+$(".btn").on("click", checkPlatformID);
+
+function checkPlatformID(event){
+   event.preventDefault();
+   
+   var platformID = $(this).parent().data("platformID");
+   console.log(this);
+   var platformApiUrl =  'https://www.giantbomb.com/api/games/?api_key=073c2f94ba69540e99d2b7e8b4cd3aebb2d9befb&format=jsonp&platforms=' + platformID + '&limit=10';
+
+   console.log(platformID);
+
+   $.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    crossDomain: true,
+    jsonp: 'json_callback',
+    url: platformApiUrl
+  });
+}
+
+
+
 
 
 
 var buttonClickHandler = function (event) {
-  var platform = event.target.getattribute('data-platformID');
-
-  if(platform) {
+  var platformID = event.target.getattribute('data-platformID');
+console.log(platformID);
+  if(platformID) {
     getFeaturedPlatforms(platform);
 
     top10listEl.textContent = '';
@@ -62,8 +65,31 @@ var buttonClickHandler = function (event) {
 };
 
 
-var getFeaturedPlatforms = function (platform){
-  varapi
-}
-// FAVORITE BUTTON
+// var getFeaturedPlatforms = function (platform){
+//   var platformApiUrl =  'https://www.giantbomb.com/api/games/?api_key=073c2f94ba69540e99d2b7e8b4cd3aebb2d9befb&format=jsonp&platforms=' + platformID + '&limit=10';
+// console.log(platformApiUrl)
+//   $.ajax({
+//     type: 'GET',
+//     dataType: 'jsonp',
+//     crossDomain: true,
+//     jsonp: 'json_callback',
+//     url: platformApiUrl
+//   })
+  
+//   .then(function(data){
+//     console.log(data);
+//   });    
+// }
+// var getFeaturedRepos = function (language) {
+//   var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
 
+//   fetch(apiUrl).then(function (response) {
+//     if (response.ok) {
+//       response.json().then(function (data) {
+//         displayRepos(data.items, language);
+//       });
+//     } else {
+//       alert('Error: ' + response.statusText);
+//     }
+//   });
+// };
