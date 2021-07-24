f8ef248a13e497d31625fae3da53278e4a2c6387
Ryan McCarthy <rmmcc92@aol.com>
2021-07-24 14:15:25 -0400

    fixed everything in the lists

--- favoriteList.js
+++ favoriteList.js
@@ -1,24 +1,4 @@
-var Button1 = $(".game-1-button");
-var Button2 = $(".game-2-button");
-var Button3 = $(".game-3-button");
-var Button4 = $(".game-4-button");
-var Button5 = $(".game-5-button");
-var Button6 = $(".game-6-button");
-var Button7 = $(".game-7-button");
-var Button8 = $(".game-8-button");
-var Button9 = $(".game-9-button");
-var Button10 = $(".game-10-button");
 
-var game1El = $("#game1");
-var game2El = $("#game2");
-var game3El = $("#game3");
-var game4El = $("#game4");
-var game5El = $("#game5");
-var game6El = $("#game6");
-var game7El = $("#game7");
-var game8El = $("#game8");
-var game9El = $("#game9");
-var game10El = $("#game10");
 
 var topTen = $(".top-10-games");
 var savedListEl = $(".saved-list");
@@ -35,6 +15,42 @@ function pageLoad() {
 
 };
 
+firstListItem()
+function firstListItem() {
+   data = JSON.parse(localStorage.getItem('data'));
+    var guid3 = data.results[0].guid
+
+    // api call for title, genre, and description
+    var singleGameAPI = 'https://www.giantbomb.com/api/game/' + guid3 + '/?api_key=073c2f94ba69540e99d2b7e8b4cd3aebb2d9befb&format=jsonp';
+    $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        crossDomain: true,
+        jsonp: 'json_callback',
+        url: singleGameAPI
+    })
+
+        .then((data) => {
+            var titleValue = data.results.name;
+            var descValue = data.results.deck;
+            var genreValue = data.results.genres ? data.results.genres[0].name : "Game genre not available";
+            var ratingValue = data.results.original_game_rating ? data.results.original_game_rating[0].name : "Game rating not available";
+
+            var imageURL = data.results.image.medium_url;
+            $('#photoBox').empty()
+            console.log(imageURL);
+            var image = new Image();
+            image.src = imageURL;
+            document.getElementById('photoBox').appendChild(image);
+
+
+            title.innerHTML = titleValue;
+            gameDesc.innerHTML = descValue;
+            gameGenre.innerHTML = "Genre: " + genreValue;
+            gameRating.innerHTML = "Rating: " + ratingValue;
+        })
+}
+
 
 var data = JSON.parse(localStorage.getItem('data'));
 console.log("data: ", data);
@@ -44,7 +60,8 @@ topTenAppend()
 function topTenAppend() {
     for (i = 0; i < data.results.length; i++) {
         var gameEl = "#game" + [i + 1]
-        topTen.append("<a href='#!'class='collection-item black darken-1 white-text' data-index='" + [i] + "' id = 'game" + [i + 1] + "'>" + data.results[i].name + "</a>")
+        topTen.append("<a href='#!'class='collection-item black darken-1 white-text' data-index='" + [i] + "' id = 'game" + [i + 1] + "'data-guid='" + data.results[i].guid + "'>" + data.results[i].name + "</a>")
+        localStorage.setItem (data.results[i].name , data.results[i].guid)
         var gameBtn = "<a class='btn-floating btn-small waves-effect waves-light deep-purple accent-3 game-" + [i + 1] + "-button'><i class='material-icons'>add</i></a>"
         $(gameEl).append(gameBtn)
 
@@ -52,8 +69,7 @@ function topTenAppend() {
 }
 
 $(topTen).on('click', '.btn-small', function () {
-    console.log($(this));
-    var selection = ($(this).parent().text())
+    var selection = ($(this).parent().text().replace("add", ""))
     localStorage.setItem(listCount, selection);
     var listItem = $(".saved-list");
     listItem.append("<a href='#!'class='collection-item black darken-1 white-text'>" + selection + "</a>");
@@ -103,7 +119,7 @@ $(topTen).on('click', 'a', function () {
 $(savedListEl).on('click', 'a', function () {
     console.log($(this).attr('data-index'));
     var index = $(this).attr('data-index');
-    var guid2 = $(this).attr("data-guid");
+    var guid2 = localStorage.getItem($(this).text())
 
     // api call for title, genre, and description
     var singleGameAPI = 'https://www.giantbomb.com/api/game/' + guid2 + '/?api_key=073c2f94ba69540e99d2b7e8b4cd3aebb2d9befb&format=jsonp';
@@ -143,11 +159,3 @@ for (i = 0; i < localStorageCount; i++) {
     listItem.append("<a href='#!'class='collection-item black darken-1 white-text' data-guid='" + data.results[i].guid + "'>" + savedItem + "</a>");
 }
 
-
-//Append Image and Videos to Game Details
-
-// var imageURL = data.results[0].image.medium_url;
-// console.log(imageURL);
-// var image = new Image();
-// image.src = imageURL;
-// document.getElementById('photoBox').appendChild(image);
